12a19919398114f4a4ccb26121b1a694a147b360
shyndman <scotty.hyndman@gmail.com>
2012-04-25 19:16:46 -0400

    Improved documentation

--- support/schedule.rb
+++ support/schedule.rb
@@ -1,4 +1,19 @@
-configure :using_env
+# CONFIGURATION
+
+# By default, configuration values are supplied by environment variables
+configure :using_env 
+
+# You can optionally replace the line above with values in place
+# configure :host => "",
+#           :twilio_sid => "",
+#           :twilio_auth_token => "",
+#           :contextio_account => "",
+#           :contextio_key => "",
+#           :contextio_secret => ""
+
+
+# SETS YOUR LOCAL TIMEZONE
+
 local_utc_offset %{utc_offset}
 
 # DEFINE YOUR CONTACTS (change this to be your contacts -- but first add us on Twitter :)
@@ -10,7 +25,7 @@ contact :victor, "416-555-4321" # @vimota
 
 # DEFINE WHAT EMAILS YOU WANT TO WATCH FOR (regular expressions and exact string matches supported)
 
-match :subject => /server down/ # rackspace ping
+match :subject => /server down/ # external server ping service
 match :subject => /resource limit reached/ # monit
 
 
