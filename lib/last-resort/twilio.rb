e03e7edddc6293d7759e29f35e63d6b6c2bdfea3
shyndman <scotty.hyndman@gmail.com>
2012-05-02 19:39:06 -0400

    Tabs are now spaces....IAAAAANN

--- lib/last-resort/twilio.rb
+++ lib/last-resort/twilio.rb
@@ -8,65 +8,65 @@ HOST = ""
 
 module LastResort
 
-	class Contact
-		attr_accessor :name, :number
+  class Contact
+    attr_accessor :name, :number
 
-		def initialize(name = "", number = "")
-			@name = name
-			@number = number
-		end
-	end
+    def initialize(name = "", number = "")
+      @name = name
+      @number = number
+    end
+  end
 
-	# Represents the email notification (exception) from ContextIO.
-	# Created when our webhook is called from ContextIO.
-	class ExceptionSession
+  # Represents the email notification (exception) from ContextIO.
+  # Created when our webhook is called from ContextIO.
+  class ExceptionSession
 
-		# Array of strings representing numbers
-		attr_accessor :contacts, :client, :call, :index, :description, :handled
+    # Array of strings representing numbers
+    attr_accessor :contacts, :client, :call, :index, :description, :handled
 
-		def initialize(contacts = [], description = "A general exception has occurred")
-			@contacts = contacts
-			@description = description
-			@client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
-			@index = -1
-			@handled = false
-		end
+    def initialize(contacts = [], description = "A general exception has occurred")
+      @contacts = contacts
+      @description = description
+      @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+      @index = -1
+      @handled = false
+    end
 
-	  # Call the next Contact in the queue. Returns false if call was not made
-		def call_next
-			@index += 1
+    # Call the next Contact in the queue. Returns false if call was not made
+    def call_next
+      @index += 1
 
-			return false if @handled || @index >= @contacts.size
+      return false if @handled || @index >= @contacts.size
 
-			# Make the call
-			@call = @client.account.calls.create(
-			  :from => FROM_NUMBER,
-			  :to => @contacts[@index].number,
-			  :url => "http://#{HOST}/twilio/call",
-			  :status_callback => "http://#{HOST}/twilio/status_callback"
-			)
+      # Make the call
+      @call = @client.account.calls.create(
+        :from => FROM_NUMBER,
+        :to => @contacts[@index].number,
+        :url => "http://#{HOST}/twilio/call",
+        :status_callback => "http://#{HOST}/twilio/status_callback"
+      )
 
-			return true
-		end
+      return true
+    end
 
-		# Called when someone in the queue has handled this
-		def end
-			@handled = true
-		end
+    # Called when someone in the queue has handled this
+    def end
+      @handled = true
+    end
 
-		# Begin the notification cycle
-		def notify
-			self.call_next
-		end
+    # Begin the notification cycle
+    def notify
+      self.call_next
+    end
 
-		# Name of the latest callee (latest call)
-		def callee_name
-			@contacts[@index].name
-		end
+    # Name of the latest callee (latest call)
+    def callee_name
+      @contacts[@index].name
+    end
 
-		# Number of latest callee (latest call)
-		def callee_number
-			@contacts[@index].number
-		end
-	end
+    # Number of latest callee (latest call)
+    def callee_number
+      @contacts[@index].number
+    end
+  end
 end
